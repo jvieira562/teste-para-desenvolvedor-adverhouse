ea2d1e081450f86ffabff6bb98511340772dd4fa
jvieira <contato.josevictorvieira@gmail.com>
2023-07-06 12:22:14 -0300

    Ajsustando arquivos para semrem exibidos em repositorio publicos

--- Timesheet/Timesheet.Data/Connection/DatabaseConnection.cs
+++ Timesheet/Timesheet.Data/Connection/DatabaseConnection.cs
@@ -12,7 +12,7 @@ namespace Timesheet.Data.Connection
 
         public DatabaseConnection()
         {
-            string connectionString = "Server=azure-server-mysql.mysql.database.azure.com;Port=3306;Database=testeadverhouse;Uid=sysadmin;Pwd=@TesTe201452;SslMode=Required;";
+            string connectionString = "Server=ENDERECO;Port=3306;Database=SEU_BANCO;Uid=usuario;Pwd=SENHA;SslMode=Required;";
             Connection = new MySqlConnection(connectionString);
             Connection.Open();
         }
